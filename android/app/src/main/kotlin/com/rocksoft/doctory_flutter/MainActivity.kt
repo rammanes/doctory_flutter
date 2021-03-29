cdba0c2c3565aa0743d831e34a7e785442663eaa
Sara Seif <sara.altelmsany@gmail.com>
2021-03-29 21:07:24 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/rocksoft/doctory_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rocksoft.doctory_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Splash/Splash.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class Splash extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+  
+}class SplashState extends State<Splash>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
